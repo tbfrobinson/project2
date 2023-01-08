40778c09e38c27ee1d8212511a0016c68d8f6c58
tbfrobinson <tbfrobinson@gmail.com>
2023-01-08 14:55:05 -0800

    attempt at lucky button

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+console.log('in')
+
+const random = Math.floor(Math.random() * 60000)
+
+const lucky = document.querySelector('.feelin-lucky')
+
+// lucky.addEventListener('click', doThis())
+
+// function doThis(){
+//     console.log('click')
+// }
